8a983d3b30c96babd82a190af316b59412fc5e81
TechQuery <shiy2008@gmail.com>
2023-04-23 22:31:56 +0800

    [add] REST Form component [add] Row Editing dialog of REST Table component

--- src/component/RestTable.tsx
+++ src/component/RestTable.tsx
@@ -1,17 +1,20 @@
-import { Table, TableProps } from 'antd';
+import { EditOutlined } from '@ant-design/icons';
+import { Button, Modal, Table, TableProps } from 'antd';
 import { ColumnType } from 'antd/lib/table';
+import { observable } from 'mobx';
 import { observer } from 'mobx-react';
-import { DataObject, ListModel } from 'mobx-restful';
-import { HTMLInputTypeAttribute, PureComponent } from 'react';
+import { DataObject, IDType, ListModel } from 'mobx-restful';
+import { PureComponent } from 'react';
 
-export interface Column<T extends DataObject> extends ColumnType<T> {
-  type?: HTMLInputTypeAttribute;
-}
+import { Field, RestForm } from './RestForm';
+
+export type Column<T extends DataObject> = Omit<ColumnType<T>, 'key'> & Field;
 
 export interface RestTableProps<T extends DataObject>
   extends Omit<TableProps<T>, 'columns'> {
   columns: Column<T>[];
   store: ListModel<T>;
+  editable?: boolean;
 }
 
 /**
@@ -21,28 +24,64 @@ export interface RestTableProps<T extends DataObject>
 export class RestTable<T extends DataObject> extends PureComponent<
   RestTableProps<T>
 > {
+  @observable
+  editingId?: IDType;
+
   componentDidMount() {
     this.props.store.getList({}, 1);
   }
 
-  get columns(): ColumnType<T>[] {
-    return this.props.columns.map(
-      ({ key, dataIndex = key, type, title = key, render, ...column }) => ({
-        ...column,
-        key,
-        dataIndex,
-        title,
-        render:
-          render ||
-          (value =>
-            type === 'url' ? (
-              <a target="_blank" href={value}>
-                {value}
-              </a>
-            ) : (
-              value
-            ))
-      })
+  closeEditor = () => (this.editingId = undefined);
+
+  get columns(): Column<T>[] {
+    const { columns, store, editable } = this.props;
+
+    return [
+      ...columns.map(
+        ({ key, dataIndex = key, type, title = key, render, ...column }) => ({
+          ...column,
+          key,
+          dataIndex,
+          title,
+          type,
+          render:
+            render ||
+            (value =>
+              type === 'url' ? (
+                <a target="_blank" href={value}>
+                  {value}
+                </a>
+              ) : (
+                value
+              ))
+        })
+      ),
+      editable && {
+        key: 'edit',
+        render: (_, { [store.indexKey]: ID }) => (
+          <Button onClick={() => (this.editingId = ID)}>
+            <EditOutlined />
+          </Button>
+        )
+      }
+    ];
+  }
+
+  renderDialog() {
+    const { store } = this.props,
+      { columns, editingId } = this;
+    const editing = this.editingId != null,
+      currentTitle = store.currentOne[store.indexKey];
+
+    return (
+      <Modal
+        title={currentTitle}
+        open={editing}
+        onOk={this.closeEditor}
+        onCancel={this.closeEditor}
+      >
+        {editing && <RestForm id={editingId} fields={columns} store={store} />}
+      </Modal>
     );
   }
 
@@ -51,19 +90,27 @@ export class RestTable<T extends DataObject> extends PureComponent<
     const { downloading, pageSize, pageIndex, totalCount, currentPage } = store;
 
     return (
-      <Table
-        {...props}
-        columns={this.columns}
-        rowKey={store.indexKey.toString()}
-        loading={downloading > 0}
-        pagination={{
-          pageSize,
-          current: pageIndex,
-          total: totalCount,
-          onChange: (page, pageSize) => store.getList({}, page, pageSize)
-        }}
-        dataSource={currentPage}
-      />
+      <>
+        <header className="d-flex justify-content-end">
+          <Button type="primary" onClick={() => (this.editingId = 0)}>
+            +
+          </Button>
+        </header>
+        <Table
+          {...props}
+          columns={this.columns}
+          rowKey={store.indexKey.toString()}
+          loading={downloading > 0}
+          pagination={{
+            pageSize,
+            current: pageIndex,
+            total: totalCount,
+            onChange: (page, pageSize) => store.getList({}, page, pageSize)
+          }}
+          dataSource={currentPage}
+        />
+        {this.renderDialog()}
+      </>
     );
   }
 }
